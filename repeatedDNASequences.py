8dd1791ef0439e329247402c910e402c9df4173d
mengzhehuang <mengzhehuang@gmail.com>
2020-09-21 23:31:14 -0400

    "longestCommonSubstring_brute_force_DP"

---
+++ repeatedDNASequences.py
@@ -0,0 +1,19 @@
+def repeatedDNASequences(s):
+    if len(s) < 10:
+        return []
+    power = pow(26, 9)
+    m = {}
+    res = set()
+    cur_hash = 0
+    for i in range(9):
+        cur_hash = cur_hash * 26 + ord(s[i]) - ord('A')
+
+    for i in range(9, len(s)):
+        cur_hash = cur_hash * 26 + ord(s[i]) - ord('A')
+        if cur_hash in m:
+            res.add(m[cur_hash])
+        else:
+            m[cur_hash] = s[i - 9:i + 1]
+        cur_hash = cur_hash - (ord(s[i - 9]) - ord('A')) * power
+
+    return sorted(list(res))
