a937c2dc441fe0de1244e69d8594c4d3bc12ede5
mengzhehuang <mengzhehuang@gmail.com>
2020-10-30 13:57:39 -0400

    "optimalStockBasket.py"

---
+++ optimalStockBasket.py
@@ -0,0 +1,21 @@
+def optimalStockBasket(stocks, riskBudget):
+    n = len(stocks)
+    res = 0
+
+    def helper(offset, cur_return, cur_risk):
+        nonlocal res
+        if cur_risk > riskBudget:
+            return
+        if offset == n:
+            res = max(res, cur_return)
+            return
+
+            ## not take this stock
+        helper(offset + 1, cur_return, cur_risk)
+
+        ret, risk = stocks[offset]
+        ## take this stock
+        helper(offset + 1, cur_return + ret, cur_risk + risk)
+
+    helper(0, 0, 0)
+    return res
